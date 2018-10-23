d0c39a7d2ef24354a1f40fb94f0c8c4d0158ae1f
hamyogi <shinzzang1@gmail.com>
2018-10-23 17:50:38 +0900

    first commit

--- app/views.py
+++ app/views.py
@@ -10,3 +10,6 @@ def index(request):
 def detail(request, num):
     item = Notice.objects.get(id=num)   #id=1
     return render(request, 'app/detail.html', {'item':item})
+
+def about(request) :
+    return render(request, 'app/about.html', {})
