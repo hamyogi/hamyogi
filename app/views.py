631c50e594bd5e2b1457dace59db128db176ddba
hamyogi <shinzzang1@gmail.com>
2018-10-23 16:32:14 +0900

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Notice
+
+
+def index(request):
+    notice = Notice.objects.all()
+    return render(request, 'app/index.html', {'list': notice})
+
+
+def detail(request, num):
+    item = Notice.objects.get(id=num)   #id=1
+    return render(request, 'app/detail.html', {'item':item})
